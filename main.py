3d628376e30802455e9a2698f18228e32dbc5a94
comLlama <alphalupus846@gmail.com>
2021-10-17 19:48:29 +0200

    Input handling done

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-for i in range(4):
-    print("Hello for the {0}. time".format(i))
+p = int(input())
+n = int(input())
+e = int(input())
+#print("p={0}, n={1}, e={2}".format(p,n,e))
+
+input()
+routes = []
+for i in range(p):
+    line = input().split("\t")
+    line[0] = int(line[0])
+    line[1] = int(line[1])
+    routes.append(line)
+
+input()
+crossroads = []
+for i in range(n):
+    line = input().split("\t")
+    line[0] = int(line[0])
+    line[1] = int(line[1])
+    crossroads.append(line)
+
+input()
+roads = []
+for i in range(e):
+    line = input().split("\t")
+    line[0] = int(line[0])
+    line[1] = int(line[1])
+    roads.append(line)
