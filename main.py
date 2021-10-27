cf0a926b0b9cd5b545278c8f069ebfd16b10a844
comLlama <alphalupus846@gmail.com>
2021-10-27 13:05:42 +0200

    Created a usable data structure

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import math
+
+class Node:
+    def __init__(self, x=None, y=None):
+        self.x = x
+        self.y = y
+        self.ne = []
+
+    def getDistance(self, idx):
+        return math.sqrt(((self.x - self.ne[idx].x)**2) + ((self.y - self.ne[idx].y)**2))
+    
+
 p = int(input())
 n = int(input())
 e = int(input())
-#print("p={0}, n={1}, e={2}".format(p,n,e))
 
 input()
-routes = [] # from - to nodes
+routes = [] # from - to nodes TO SEARCH
 for i in range(p):
     line = input().split("\t")
     line[0] = int(line[0])
@@ -12,17 +23,21 @@ for i in range(p):
     routes.append(line)
 
 input()
-crossroads = [] # nodes
+nodes = []
+#crossroads = [] # nodes
 for i in range(n):
     line = input().split("\t")
     line[0] = int(line[0])
     line[1] = int(line[1])
-    crossroads.append(line)
+    nodes[i] = Node(line[0], line[1])
+    #crossroads.append(line)
 
 input()
-roads = [] # edges
+#roads = [] # edges
 for i in range(e):
     line = input().split("\t")
     line[0] = int(line[0])
     line[1] = int(line[1])
-    roads.append(line)
+    nodes[line[0]].ne.append(nodes[line[1]])
+    nodes[line[1]].ne.append(nodes[line[0]])
+    #roads.append(line)
