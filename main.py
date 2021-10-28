06b5d24738da3e87f18fa9d5f9b432c7b25c5acd
comLlama <alphalupus846@gmail.com>
2021-10-28 18:51:25 +0200

    Task Completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import copy
 
 class Node:
     def __init__(self, index, x=None, y=None):
@@ -7,32 +8,64 @@ class Node:
         self.y = y
         self.ne = []
 
-    def getNeighbourDistance(self, idx):
-        return calcDistance(self.x, self.ne[idx].x, self.y, self.ne[idx].y)
-
     def getManhattanDistance(self, node):
         return calcDistance(self.x, node.x, self.y, node.y)
 
-class TracedNodes:
-    def __init__(self, start, end):
-        self.traced = [[start, 0, start.getManhattanDistance(end)]]
-        #   node, distance from start, heuristic distance to end
-        self.end = end
+class NodeScore:
+    def __init__(self, node, dist, heur):
+        self.node = node
+        self.dist = dist
+        self.heur = heur
 
-    def reorder(self):
-        self.traced = self.traced.sort(key=lambda x: x[1]+x[2])
+    def getScore(self):
+        return self.dist + self.heur
 
 
+#self.traced = self.traced.sort(key=lambda x: x[1]+x[2])
     
 def calcDistance(x1, x2, y1, y2):
     return math.sqrt(((x1 - x2)**2) + ((y1 - y2)**2))
 
+def calcNodeDistance(node1, node2):
+    return calcDistance(node1.x, node2.x, node1.y, node2.y)
 
-def routeDistance(nodes, startNodeIdx, endNodeIdx):
+
+def routeDistance(nodes, startNodeIdx, endNodeIdx): #has redundancy in node scores
     endNode = nodes[endNodeIdx]
-    currentNodes = [nodes[startNodeIdx]]
-    currentNodes
-    return 0
+    startNode = nodes[startNodeIdx]
+    nodeScores = [NodeScore(startNode, 0, startNode.getManhattanDistance(endNode))]
+    while (nodeScores[0].node != endNode):
+        for neighbour in nodeScores[0].node.ne:
+            nodeScores.append(NodeScore(neighbour, calcNodeDistance(nodeScores[0].node, neighbour),neighbour.getManhattanDistance(endNode)))
+        nodeScores.sort(key=lambda x: x.dist + x.heur)
+    return nodeScores[0].dist
+
+def routeDistance2(nodes, startNodeIdx, endNodeIdx):
+    lnodes = copy.copy(nodes)
+    #print("{0}, {1}".format(len(lnodes), endNodeIdx))
+    endNode = lnodes[endNodeIdx]
+    nodeScores = []
+    for i in range(len(lnodes)):
+        nodeScores.append(NodeScore(None, math.inf, math.inf))
+    nodeScores[startNodeIdx].dist = 0
+    nodeScores[startNodeIdx].heur = lnodes[startNodeIdx].getManhattanDistance(lnodes[endNodeIdx])
+
+    tempNode = lnodes[0]
+    lnodes[0] = lnodes[startNodeIdx]
+    lnodes[startNodeIdx] = tempNode
+
+    while(lnodes[0].index != endNodeIdx): # explores all neighbour nodes instead of the cheapest one
+        for neighbour in lnodes[0].ne:
+            newdist = calcNodeDistance(neighbour, lnodes[0]) + nodeScores[lnodes[0].index].dist
+            newheur = neighbour.getManhattanDistance(endNode)
+            #print("for Node[{0}] newdist={1}, newheur={2}".format(neighbour.index, newdist, newheur))
+            if (nodeScores[neighbour.index].getScore() > newdist + newheur):
+                nodeScores[neighbour.index].dist = newdist
+                nodeScores[neighbour.index].heur = newheur
+        lnodes.remove(lnodes[0])
+        lnodes.sort(key=lambda x: nodeScores[x.index].getScore())
+
+    return nodeScores[endNodeIdx].dist
 
 
 p = int(input())
@@ -54,7 +87,7 @@ for i in range(n):
     line = input().split("\t")
     line[0] = int(line[0])
     line[1] = int(line[1])
-    nodes[i] = Node(i, line[0], line[1])
+    nodes.append(Node(i, line[0], line[1]))
     #crossroads.append(line)
 
 input()
@@ -68,5 +101,5 @@ for i in range(e):
     #roads.append(line)
 
 for route in routes:
-    print(routeDistance(nodes, route[0], route[1]), end="\t")
+    print("%.3f" % routeDistance2(nodes, route[0], route[1]), end="\t")
 
